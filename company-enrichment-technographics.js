cb9bcae1c2bd957474e0c00d394641dbebbd1b33
Ben <benjaminjcrane@gmail.com>
2025-12-03 23:53:24 -0500

    Create company-enrichment-technographics.js

---
+++ company-enrichment-technographics.js
@@ -0,0 +1,10 @@
+const options = {
+  method: 'POST',
+  headers: {'X-API-Key': '<api-key>', 'Content-Type': 'application/json'},
+  body: JSON.stringify({company_domain: 'gong.io'})
+};
+
+fetch('https://api.leadmagic.io/v1/companies/technographics', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
