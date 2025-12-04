73e8d7b1ba105317caca89a9f2de4284b75a4dcd
Ben <benjaminjcrane@gmail.com>
2025-12-04 00:17:14 -0500

    Delete jobs-region.js

--- jobs-region.js
+++
@@ -1,6 +0,0 @@
-const options = {method: 'GET', headers: {'X-API-Key': '<api-key>'}};
-
-fetch('https://api.leadmagic.io/v1/jobs/regions', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
