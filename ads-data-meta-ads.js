a726d9b2e75d00cb4887da3d3b28a2b5b4eb24a7
Ben <benjaminjcrane@gmail.com>
2025-12-03 23:56:25 -0500

    Create ads-data-meta-ads.js

---
+++ ads-data-meta-ads.js
@@ -0,0 +1,10 @@
+const options = {
+  method: 'POST',
+  headers: {'X-API-Key': '<api-key>', 'Content-Type': 'application/json'},
+  body: JSON.stringify({company_domain: 'gong.io', company_name: 'gong'})
+};
+
+fetch('https://api.leadmagic.io/v1/ads/meta-ads-search', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
